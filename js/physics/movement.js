31fc1587be7ade4ee76d82f186b8c76ca240dfe4
bszel <147652796+bszel@users.noreply.github.com>
2025-05-02 19:08:32 +0200

    Movement update
    -Made player movement speed limit more precise, when near the limit the movement keys won't accelerate player past limit
    -When releasing left-right movement keys, the horizontal player speed decreases by half
    -The direction of the most recently pressed key is used for movement when both left and right keys are pressed
    -Removed index_old.html as it didn't have anything useful in it anymore

--- js/physics/movement.js
+++ js/physics/movement.js
@@ -1,44 +1,84 @@
 import { keyboard } from "../core/keyboard.js";
 
+const speedLimit = 3;
+const acceleration = 0.25;
+
 export class PlayerController {
     constructor(player, playerSprite) {
         this.player = player;
         this.playerSprite = playerSprite;
+        this.movementActive = false;
+        this.direction = null;
+        this.leftOld = false;
+        this.rightOld = false;
     }
 
     handleUp() {
-        if (this.player.jump && keyboard.w()) {
+        if (this.player.jump) {
             this.player.speed.y = -3;
-            this.playerSprite.playAnimation = false;
             this.player.jump = false;
         }
     }
 
     handleLeft() {
-        if (keyboard.a()) {
-            const force = this.player.speed.x > -4 ? -0.25 : 0;
-            this.player.applyImpulse({ x: force * this.player.mass, y: 0 }, this.player.position);
-            this.playerSprite.side = 0;
-            if (this.player.jump) {
-                this.playerSprite.playAnimation = true;
-            }
-        }
+        const impulse = this.player.speed.x > -speedLimit ? Math.max(-acceleration, -speedLimit - this.player.speed.x) : 0;
+        this.player.applyImpulse({ x: impulse * this.player.mass, y: 0 }, this.player.position);
+        this.playerSprite.side = 0;
     }
 
     handleRight() {
-        if (keyboard.d()) {
-            const force = this.player.speed.x < 4 ? 0.25 : 0;
-            this.player.applyImpulse({ x: force * this.player.mass, y: 0 }, this.player.position);
-            this.playerSprite.side = 1;
-            if (this.player.jump) {
-                this.playerSprite.playAnimation = true;
-            }
-        }
+        const impulse = this.player.speed.x < speedLimit ? Math.min(acceleration, speedLimit - this.player.speed.x) : 0;
+        this.player.applyImpulse({ x: impulse * this.player.mass, y: 0 }, this.player.position);
+        this.playerSprite.side = 1;
     }
 
     update() {
-        this.handleLeft();
-        this.handleRight();
-        this.handleUp();
+        const left = keyboard.a();
+        const right = keyboard.d();
+        const up = keyboard.w();
+
+        if (left || right) {
+            // Determine movement direction
+            if (!left && right) { // only right pressed
+                this.direction = 'right';
+            }
+            else if (left && !right) { // only left pressed
+                this.direction = 'left';
+            }
+            else if (left && !this.leftOld) { // left pressed this frame
+                this.direction = 'left';
+            }
+            else if (right && !this.rightOld) { // right pressed this frame
+                this.direction = 'right';
+            }
+            // if neither case is true, then both keys are pressed, and the most recently pressed key will be used
+
+            // Handle movement based on the direction
+            if (this.direction == 'right') {
+                this.handleRight();
+            }
+            else if (this.direction == 'left') {
+                this.handleLeft();
+            }
+
+            // Play running animation if player isn't jumping
+            if (this.player.jump) {
+                this.playerSprite.playAnimation = true;
+            }
+
+            this.movementActive = true;
+        }
+        else if (this.movementActive) { // Decrease player speed in the frame in which the keys are released
+            this.player.speed.x /= 2;
+            this.movementActive = false;
+        }
+
+        if (up) {
+            this.handleUp();
+        }
+
+        // Update the old state of the keys
+        this.leftOld = left;
+        this.rightOld = right;
     }
 }
